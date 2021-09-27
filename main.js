85c5ccc7bdd5d3fb60591c3438a7e7c68c740383
‘gcd’ <it_gcd@163.com>
2021-09-27 22:28:13 +0800

    流畅移动（定时器）

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 let box = document.getElementById("box");
-document.onkeydown = function(event){
-    event = event || windows.event;
-    let speed=10;
-    if(event.ctrlKey)speed =30;
-    switch(event.keyCode){
+let speed = 10;
+let dir = 0;
+setInterval(function(){
+    switch(dir){
         //左：37    上：38     右：39     下：40
         case 37:box.style.left = box.offsetLeft -speed +"px";break;
         case 39:box.style.left = box.offsetLeft +speed +"px";break;
         case 38:box.style.top = box.offsetTop -speed +"px";break;
         case 40:box.style.top = box.offsetTop +speed +"px";break;
     }
+},50);
+document.onkeydown = function(event){
+    if(event.ctrlKey)speed = 50;
+    dir = event.keyCode;
+}
+document.onkeyup = function(){
+    dir = 0
 }
